9f7251c77eb7458642229c3c7cd8de532a047498
Naman Patidar <namanpatidar309@gmail.com>
2026-09-11 09:34:20 +0530

    adding delete product

--- product-service/src/modules/product/product.routes.js
+++ product-service/src/modules/product/product.routes.js
@@ -6,13 +6,17 @@ import {
 } from "./product.controller.js";
 const router = express.Router();
 
+
 // Create Product
 router.post("/", createProductController);
+
 
 // Get All Products
 router.get("/", getAllProductsController);
 
+
 // Get Product By ID
 router.get("/:id", getProductByIdController);
+
 
 export default router;
